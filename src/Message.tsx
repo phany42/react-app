e638a20bd67df0bd4d253e26a0a8ad1ab488b58e
phany42 <phaneendraamaravati9999@gmail.com>
2025-08-13 17:46:21 +0530

    Add card-styled Message component

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-
-function Message() {
-
-    return <h1>Hello World</h1>;
+export default function Message() {
+  return (
+    <div className="card">
+      <h1>Hello World</h1>
+      <p>Welcome to my first React app </p>
+      <button>Click if you want to see</button>
+    </div>
+  );
 }
-
-export default  Message;
